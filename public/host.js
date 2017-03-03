6b963d74167c726bcb102cef70adf79fd0a5d458
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-03-03 00:49:31 -0800

    add timeout for "Detecting VR headset…" text (fixes #3)

--- public/host.js
+++ public/host.js
@@ -505,7 +505,17 @@ doc.loaded.then(function () {
     return false;
   }
 
+  var headsetsEl = webvrAgent.querySelector('#webvr-agent-headsets');
   var headsetsPresentEl = webvrAgent.querySelector('#webvr-agent-headsets-present');
+
+  setInterval(function () {
+    // headsetsEl.setAttribute('aria-hidden', webvrAgent.connectedDisplay ? false : true);
+    if (webvrAgent.connectedDisplay) {
+      headsetsPresentEl.innerHTML = 'Detecting VR headset&hellip;';
+    } else {
+      headsetsPresentEl.textContent = 'No VR headset detected';
+    }
+  }, 15000);
 
   function showConnectedDisplay (opts) {
     var displaySlug = opts.displaySlug;
@@ -574,6 +584,8 @@ doc.loaded.then(function () {
 
     html.setAttribute('data-presenting-display', displaySlug);
   }
+
+  webvr-agent-headsets
 
   window.addEventListener('message', function (evt) {
     var data = evt.data;
